cf436f1c546a23f6778c303250e815f44e34dc79
Tejas More <tejasmore660@gmail.com>
2025-03-28 18:22:52 +0530

    created book controller to add new book

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -1,3 +1,4 @@
+const BookModel = require('../models/book.js');
 
 const getAllBooks = async( req, res ) => {
 
@@ -8,7 +9,20 @@ const getSingleBookById = async( req, res ) => {
 };
 
 const addNewBook = async( req, res ) => {
-
+    try {
+        const newAddedBook = await BookModel.create(req.body);
+        if(newAddedBook){
+            res.status(201).json({
+                success : true,
+                message : "Book added sucessfully",
+                data : newAddedBook
+            });
+        };
+        
+    } catch (error) {
+        console.log("\n Error Occured while adding new book:\n", error);
+        
+    };
 };
 
 const updateBookById = async( req, res ) => {
@@ -26,4 +40,4 @@ module.exports = {
     addNewBook,
     deleteBookById
 
-}
+};
