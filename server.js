2776219b7071345ae2c1f7f4dbd86c73a280bcdd
Tejas More <tejasmore660@gmail.com>
2025-03-28 17:07:14 +0530

    creating express server and connecting server.js to .env for PORT

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
+// connecting to .env
+require('dotenv').config();
 
+const express = require('express');
+
+// creating express server
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// middleware express.json
+app.use(express.json());
+
+
+
+
+// connect via port
+app.listen(PORT,() => {
+    console.log(`\n Server is listening to port : ${PORT}`);
+});
 
 
 
